ebcc9a9bdad3e9534eccd131a60eb61fdabb3cd4
mohammadmns <mansourimohammad35@gmail.com>
2021-08-23 14:50:34 +0430

    delete SECRET_KEY

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -1,5 +1,4 @@
-
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)27nf@o7sc#94gr7xgbco2pksl)7c!whg$fm@#kjd@z27cxvr^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
