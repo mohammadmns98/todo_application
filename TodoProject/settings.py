a964eb6216a15a83fd36b23b59d86873dec8cb01
Mohammad Mansouri <mansourimohammad35@gmail.com>
2023-07-22 20:32:49 +0330

    add domain to allowed host

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -17,7 +17,9 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'todo-application-bfce.onrender.com',
+]
 
 
 # Application definition
